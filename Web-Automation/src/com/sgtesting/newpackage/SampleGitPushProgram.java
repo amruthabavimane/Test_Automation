53f498ee1d5fa547070443fd7d064b4b12c24b86
amruthabavimane <amruthhatech@gmail.com>
2021-06-06 20:36:41 +0530

    NewTestScript

---
+++ Web-Automation/src/com/sgtesting/newpackage/SampleGitPushProgram.java
@@ -0,0 +1,9 @@
+package com.sgtesting.newpackage;
+
+public class SampleGitPushProgram {
+
+	public static void main(String[] args)
+	{
+		System.out.println("This is a demo Git Push command");
+	}
+}
